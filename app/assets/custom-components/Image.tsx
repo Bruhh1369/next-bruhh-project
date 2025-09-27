caebfb10362ba9a81d95850924e285039b6291cb
Bruhh1369 <bruhh136969@gmail.com>
2025-09-27 21:42:30 +0700

    ff

--- app/assets/custom-components/Image.tsx
+++ app/assets/custom-components/Image.tsx
@@ -9,6 +9,27 @@ interface ImgProps {
 }
 
 const Img = ({ src, alt, fullSize = false, imgWidth = 300, imgHeigth = 300 }: ImgProps) => {
+
+    const gif = src.endsWith('.gif');
+
+    if (gif) return (
+        <div className="img">
+            {fullSize ? <Image
+                src={src}
+                alt={alt}
+                fill
+                unoptimized
+            /> :
+                <Image
+                    src={src}
+                    alt={alt}
+                    width={imgWidth}
+                    height={imgHeigth}
+                    unoptimized
+                />}
+        </div>
+    )
+
     return (
         <div className="img">
             {fullSize ? <Image
